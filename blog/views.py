675b50f9625467072c63ac51b24f2de5562ddf0b
kyun <839530159@qq.com>
2019-12-22 14:45:26 +0800

    具体博客内容界面开发

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Category, Banner, Article, Tag, Link
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def hello(request):
@@ -30,19 +31,72 @@ def index(request):
 
 #列表页
 def list(request,lid):
-    pass
+
+    list = Article.objects.filter(category_id=lid) 
+    page = request.GET.get('page')
+    paginator = Paginator(list, 5)
+    try:
+        list = paginator.page(page)
+    except PageNotAnInteger:
+        list = paginator.page(1)
+    except EmptyPage:
+        list = paginator.page(paginator.num_pages)
+
+    cname = Category.objects.get(id=lid) 
+    remen = Article.objects.filter(tui__id=2)[:6] 
+    allcategory = Category.objects.all() 
+    tags = Tag.objects.all()    
+    return render(request, 'list.html', locals())
 
 #内容页
 def show(request,sid):
-    pass
+
+    show = Article.objects.get(id=sid)
+    allcategory = Category.objects.all()
+    tags = Tag.objects.all()
+    remen = Article.objects.filter(tui__id=2)[:6]
+    hot = Article.objects.all().order_by('?')[:10]
+    previous_blog = Article.objects.filter(created_time__gt=show.created_time,category=show.category.id).first()
+    netx_blog = Article.objects.filter(created_time__lt=show.created_time,category=show.category.id).last()
+    show.views = show.views + 1
+    show.save()
+    return render(request, 'show.html', locals())
 
 #标签页
 def tag(request, tag):
-    pass
+    list = Article.objects.filter(tags__name=tag)
+    remen = Article.objects.filter(tui__id=2)[:6]
+    allcategory = Category.objects.all()
+    tname = Tag.objects.get(name=tag)
+    page = request.GET.get('page')
+    tags = Tag.objects.all()
+    paginator = Paginator(list, 5)
+    try:
+        list = paginator.page(page)  
+    except PageNotAnInteger:
+        list = paginator.page(1)  
+    except EmptyPage:
+        list = paginator.page(paginator.num_pages)  
+    return render(request, 'tags.html', locals())
 
 # 搜索页
 def search(request):
-    pass
+    ss=request.GET.get('search')
+    list = Article.objects.filter(title__icontains=ss)
+    remen = Article.objects.filter(tui__id=2)[:6]
+    allcategory = Category.objects.all()
+    page = request.GET.get('page')
+    tags = Tag.objects.all()
+    paginator = Paginator(list, 10)
+    try:
+        list = paginator.page(page) 
+    except PageNotAnInteger:
+        list = paginator.page(1) 
+    except EmptyPage:
+        list = paginator.page(paginator.num_pages)
+    return render(request, 'search.html', locals())
+
 # 关于我们
 def about(request):
-    pass
+    allcategory = Category.objects.all()
+    return render(request, 'page.html',locals())
